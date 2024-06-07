50484ecb6e5dfc44af171385d32de3a476824102
luis11! <131817446+luispneu@users.noreply.github.com>
2024-06-07 16:07:51 -0400

    Add files via upload

---
+++ src/app/Componentes/Header.js
@@ -0,0 +1,11 @@
+export function Header() {
+    return (
+        <div>
+            <img src="https://i.pinimg.com/236x/5d/26/a1/5d26a173f443cbd190e34481438d474b.jpg" width={100} height={100} style={{ float: "right" }} />
+            <img src="https://img.freepik.com/premium-vector/shark-logo_647943-151.jpg" width={100} height={100}/>
+            <button>ADICIONAR</button>
+            <p>Venha conhecer a nossa loja!!</p>
+        </div>
+    );
+}
+
